1817e503e16763365e23b275056ce95e56b5c4a4
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2026-01-26 23:40:50 +0545

    feat: initialize client application with React and Vite

---
+++ dayaluhaat_client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    // This is still needed for the 'simple-peer' video call library
+    'global': 'globalThis',
+  },
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+  }
+});
