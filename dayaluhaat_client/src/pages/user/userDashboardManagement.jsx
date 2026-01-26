b5982280d12789358f9db0110b755f556bf4161c
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2026-01-26 23:41:03 +0545

    feat: add user dashboard features and profile management

---
+++ dayaluhaat_client/src/pages/user/userDashboardManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import userDashboard from "../../components/user/UserDashboard"
+
+export default function userDashboard() {
+  return (
+    <div>userDashboard</div>
+  )
+}
+
+
