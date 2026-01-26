95f602ce721862302ed32cc37151bd58863c1426
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2026-01-26 23:40:56 +0545

    feat: implement responsive layout structures for user and admin

---
+++ dayaluhaat_client/src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/header"
+import Footer from "../components/footer"
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  )
+}
+
+export default MainLayout
+
