41ba54d3c380857cebc21ed102eebfd8007170aa
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2026-01-27 22:12:43 +0545

    organize imports in main.jsx

--- dayaluhaat_client/src/main.jsx
+++ dayaluhaat_client/src/main.jsx
@@ -1,12 +1,15 @@
+// React
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
+// Third Party
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 
+// Internal
 import AuthContextProvider from './auth/AuthProvider';
 import App from './App';
-
 import './index.css';
 
 const queryClient = new QueryClient();
